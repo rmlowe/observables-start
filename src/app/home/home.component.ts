7bf1a136e2bfcfcd50b96fef421e8882df60f623
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-18 04:11:28 +0800

    Understanding Observable Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Observable, Observer, Subscription } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,12 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
+    const myNumbers = interval(1000)
+      .map(
+        (data: number) => {
+          return data * 2;
+        }
+      );
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
